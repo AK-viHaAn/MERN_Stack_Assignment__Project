d960e1ff3cae6a9fa453346cec55a867f2dbdd9e
AK-viHaAn <kanand2607755@gmail.com>
2023-03-09 13:48:29 +0530

    ulr update

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -4,10 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import CreateTask from "./createtask";
 import ManageTask from "./managetask";
-// import UserRegister from "./register/userRegister";
-// import HomePage from "./homePage"
-// import BookDetails from "./book/bookDetails";
-// import CreateBook from "./book/createBook";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
@@ -18,9 +15,7 @@ root.render(
             <Route path='/' element={<App />} />
             <Route path='/createtask' element={<CreateTask />} />
             <Route path="/managetask" element={<ManageTask />} />
-            {/* <Route path='/register' element={<UserRegister />} />
-            <Route path="/bookdetails" element={<BookDetails />} />
-            <Route path="/createbook" element={<CreateBook />} /> */}
+            
         </Routes>
     </BrowserRouter>
 )
